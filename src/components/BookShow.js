f527819fbf73b779f9ae83c4544eb04cf5eee624
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-07-26 23:54:03 +0530

    form

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,11 +1,24 @@
-function BookShow({book,onDelete}){
-    const handleClick=()=>{
+import BookEdit from './BookEdit'
+import {useState} from "react";
+function BookShow({book,onDelete,onEdit}){
+    const[showEdit,setShowEdit]=useState(false);
+const handleEditClick=()=>{
+    setShowEdit(!showEdit);
+}
+
+    const handleDeleteClick=()=>{
         onDelete(book.id);
+    }
+
+    let content=<h3>{book.title}</h3>
+    if(showEdit){
+       content=<BookEdit/> 
     }
     return(
         <div className="book-show">
-            {book.title}
-            <button className="delete" onClick={handleClick}>Delete</button>
+            {content}
+            <button className="delete" onClick={handleDeleteClick}>Delete</button>
+            <button className="edit" onClick={handleEditClick}>Edit</button>
         </div> 
     )
 };
